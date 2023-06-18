bd6c8616c13e99ad2d218afb428d6ff262414809
Olga_Belaya <protenot@yandex.ru>
2023-06-18 15:48:35 +0300

    add img and index.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ module.exports = {
   mode: "development",
   target: "web",
   devtool: "source-map",
+
   entry: {
-    main: path.resolve(__dirname, "./index.js"),
+    main: path.resolve(__dirname, "./src/index.js"),
   },
   output: {
     filename: "[name].[hash-8].js",
@@ -26,22 +27,22 @@ module.exports = {
         test: /\.(?:ico|gif|png|jpeg|jpg|svg)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name]-[hash:5][ext]",
+          filename: "img/[name]-[hash:5][ext]",
         },
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name]-[hash:5][ext]",
-        },
-      },
+      //{
+      // test: /\.(woff|woff2|eot|ttf|otf)$/,
+      // type: "asset/resource",
+      // generator: {
+      //   filename: "fonts/[name]-[hash:5][ext]",
+      // },
+      //},
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./index.html"),
+      template: path.resolve(__dirname, "./src/index.html"),
       filename: "index.html",
     }),
   ],
